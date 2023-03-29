e8aa56c1a7e802f3beef35580ec81eb330fe4781
AdvikCode22 <94784477+AdvikCode22@users.noreply.github.com>
2023-03-29 17:13:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function setup(){
+    canvas = createCanvas(550,400)
+    canvas.center()
+
+    camera = createCapture(VIDEO)
+    camera.hide()
+}
+
+function draw(){
+    image(camera,0,0,550,400)
+}
